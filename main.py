0030d591acae340f4a92e72b86db9fd8740d104a
MasterGNS <124889675+MasterGNS@users.noreply.github.com>
2023-03-22 16:02:24 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,4 @@
-from bs4 import BeautifulSoup
-import requests
-import re
+import parsing
 
-url= "https://www.omgtu.ru/university/about-the-university/persons/"
-file = open("C:\/bebra.txt","w")
-page= requests.get(url)
-print(page.status_code)
-soup = BeautifulSoup(page.text, "html.parser")
-Prep = []
-SortPrep = []
-prep = soup.findAll("div", style ="padding: 5px; font-size: 120%;")
-for n in prep:
-    if n.find(string = re.compile(" Н")) is not None:
-        SortPrep.append(n.text)
-n = 1
-for sn in SortPrep:
-    words = sn.split()
-    if words[0][0] == "Н":
-        file.write(str(n)+sn)
-        n += 1
+if __name__ == '__main__':
+    parsing.pars()
