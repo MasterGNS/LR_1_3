fe89b25305c3498d76b3917191b834ea3898b5d2
MasterGNS <124889675+MasterGNS@users.noreply.github.com>
2023-03-22 16:34:57 +0600

    Update parsing.py

--- parsing.py
+++ parsing.py
@@ -2,9 +2,15 @@ from bs4 import BeautifulSoup
 import requests
 import re
 
+def file(SortPrep):
+    file = open("bebra.txt", "w")
+    for sn in SortPrep:
+        words = sn.split()
+        if words[0][0] == "Н":
+            file.write(sn.strip() + "\n")
+
 def pars():
     url= "https://www.omgtu.ru/university/about-the-university/persons/"
-    file = open("bebra.txt","w")
     page= requests.get(url)
     print(page.status_code)
     soup = BeautifulSoup(page.text, "html.parser")
@@ -14,7 +20,4 @@ def pars():
     for n in prep:
         if n.find(string = re.compile(" Н")) is not None:
             SortPrep.append(n.text)
-    for sn in SortPrep:
-        words = sn.split()
-        if words[0][0] == "Н":
-            file.write(sn.strip()+ "\n")
+    file(SortPrep)
